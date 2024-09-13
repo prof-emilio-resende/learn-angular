3293c25c24c3ea15c86120570e65ba3f377ef89f
emilio resende <emilio.resende@coxautoinc.com>
2024-09-13 18:39:24 -0300

    Binding and services

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SigninService } from './services/SiginService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.css'
+  styleUrl: './signin.component.css',
+  providers: [SigninService]
 })
 export class SigninComponent {
+  @Input() email: string = '';
+  @Input() phone: string = '';
 
+  constructor(private svc: SigninService) { }
+
+  signIn() {
+    const result = this.svc.signIn({ email: this.email, phone: this.phone });
+    console.log(result);
+    return;
+  }
 }
