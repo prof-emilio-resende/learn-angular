6c0ee77964839ab34c683824c579b57471394155
Emilio Resende <emilio.resende@coxautoinc.com>
2024-09-13 07:24:41 -0300

    purecss + signin form

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SigninComponent } from "../signin/signin.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [SigninComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
