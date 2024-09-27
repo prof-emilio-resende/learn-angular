f5d3c8e5ac0e3e0aca9855695c0a391b687e268f
emilio resende <emilio.resende@coxautoinc.com>
2024-09-27 08:09:38 -0300

    + custom pipes

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SigninComponent } from "../signin/signin.component";
 import { User } from '../signin/models/User';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
+import { SensitiveDataPipe } from "../utils/pipes/SensitiveDataPipe";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [SigninComponent, JsonPipe],
+  imports: [SigninComponent, JsonPipe, UpperCasePipe, SensitiveDataPipe],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
